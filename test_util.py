da911aabc39f041de5e6ec46d330125a94f3757b
Frank Battaglia <frank@Franks-MacBook-Pro.local>
2013-10-23 11:54:55 -0400

    add more utility functions

--- test_util.py
+++ test_util.py
@@ -1,26 +1,270 @@
+import gc
 import unittest
-import util
+import objgraph
+import weakref
+from util import (none_or_equal, iterablate, check_signature,
+                  WeaklyBoundCallable)
 
 
-class TestUtil(unittest.TestCase):
+class TestNoneOrEual(unittest.TestCase):
+
+    def test_none_or_equal(self):
+        self.assertTrue(none_or_equal(None, True))
+        self.assertTrue(none_or_equal(None, False))
+        self.assertTrue(none_or_equal(None, None))
+        self.assertFalse(none_or_equal(False, True))
+        self.assertTrue(none_or_equal(False, False))
+        self.assertFalse(none_or_equal(False, None))
+        self.assertTrue(none_or_equal(True, True))
+        self.assertFalse(none_or_equal(True, False))
+        self.assertFalse(none_or_equal(True, None))
+
+
+class TestIterablate(unittest.TestCase):
 
     def test_terablate(self):
         class FancyList(list):
             pass
 
-        self.assertEqual(util.iterablate(None), ())
-        self.assertEqual(util.iterablate(None, wrapper_cls=list), [])
-        self.assertEqual(util.iterablate([]), [])
-        self.assertEqual(util.iterablate([], also_wrap=list), ([],))
-        self.assertEqual(util.iterablate(FancyList()), FancyList())
-        self.assertEqual(util.iterablate(FancyList(), also_wrap=list),
+        self.assertEqual(iterablate(None), ())
+        self.assertEqual(iterablate(None, wrapper_cls=list), [])
+        self.assertEqual(iterablate([]), [])
+        self.assertEqual(iterablate([], also_wrap=list), ([],))
+        self.assertEqual(iterablate(FancyList()), FancyList())
+        self.assertEqual(iterablate(FancyList(), also_wrap=list),
                          (FancyList(),))
-        self.assertEqual(util.iterablate(FancyList(), also_wrap=FancyList),
+        self.assertEqual(iterablate(FancyList(), also_wrap=FancyList),
                          (FancyList(),))
-        self.assertEqual(util.iterablate([], also_wrap=FancyList), [])
-        self.assertEqual(util.iterablate(1), (1,))
-        self.assertEqual(util.iterablate('foo'), ('foo',))
-        self.assertEqual(util.iterablate(('foo', 'bar')), ('foo', 'bar'))
+        self.assertEqual(iterablate([], also_wrap=FancyList), [])
+        self.assertEqual(iterablate(1), (1,))
+        self.assertEqual(iterablate('foo'), ('foo',))
+        self.assertEqual(iterablate(('foo', 'bar')), ('foo', 'bar'))
+
+
+class TestValidateUnboundCallableSignature(unittest.TestCase):
+
+    def test_min_less_than_max(self):
+        try:
+            check_signature((lambda *args: None), min_args=0, max_args=2)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature((lambda *args: None), min_args=3, max_args=2)
+
+    def test_min_args_sufficient(self):
+        c = lambda x, y, z=None, *args: None
+        try:
+            check_signature(c, min_args=2)
+            check_signature(c, min_args=3)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature(c, min_args=0)
+        with self.assertRaises(Exception):
+            check_signature(c, min_args=1)
+
+    def test_max_args_fit(self):
+        c = lambda x=None, y=None, z=None: None
+        try:
+            check_signature(c, max_args=0)
+            check_signature(c, max_args=1)
+            check_signature(c, max_args=2)
+            check_signature(c, max_args=3)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature(c, max_args=4)
+        with self.assertRaises(Exception):
+            check_signature(c)
+
+    def test_num_args_values(self):
+        try:
+            check_signature((lambda x: None), num_args=1)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature((lambda x: None), min_args=1)
+        with self.assertRaises(Exception):
+            check_signature((lambda x: None), max_args=1)
+        with self.assertRaises(Exception):
+            check_signature((lambda x, y: None), num_args=1)
+        with self.assertRaises(Exception):
+            check_signature((lambda: None), num_args=1)
+
+    def test_min_default(self):
+        try:
+            check_signature((lambda: None), max_args=0)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature((lambda x: None), max_args=1)
+
+    def test_max_default(self):
+        try:
+            check_signature((lambda *args: None), min_args=0)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature((lambda: None), min_args=0)
+
+
+class TestValidateBoundCallableSignature(unittest.TestCase):
+
+    def method_0(self):
+        pass
+
+    def method_1(self, arg1):
+        pass
+
+    def method_2(self, arg1, arg2):
+        pass
+
+    @classmethod
+    def classmethod_0(cls):
+        pass
+
+    @classmethod
+    def classmethod_1(cls, arg1):
+        pass
+
+    @classmethod
+    def classmethod_2(cls, arg1, arg2):
+        pass
+
+    @staticmethod
+    def staticmethod_0():
+        pass
+
+    @staticmethod
+    def staticmethod_1(arg1):
+        pass
+
+    @staticmethod
+    def staticmethod_2(arg1, arg2):
+        pass
+
+    def test_instance_methods(self):
+        try:
+            check_signature(self.method_0, num_args=0)
+            check_signature(self.method_1, num_args=1)
+            check_signature(self.method_2, num_args=2)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature(self, method_0, min_args=1)
+        with self.assertRaises(Exception):
+            check_signature(self, method_1, min_args=2)
+        with self.assertRaises(Exception):
+            check_signature(self, method_2, min_args=3)
+
+    def test_class_methods(self):
+        try:
+            check_signature(self.classmethod_0, num_args=0)
+            check_signature(self.classmethod_1, num_args=1)
+            check_signature(self.classmethod_2, num_args=2)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature(self, classmethod_0, min_args=1)
+        with self.assertRaises(Exception):
+            check_signature(self, classmethod_1, min_args=2)
+        with self.assertRaises(Exception):
+            check_signature(self, classmethod_2, min_args=3)
+
+    def test_static_methods(self):
+        try:
+            check_signature(self.staticmethod_0, num_args=0)
+            check_signature(self.staticmethod_1, num_args=1)
+            check_signature(self.staticmethod_2, num_args=2)
+        except AssertionError:
+            self.fail()
+        with self.assertRaises(Exception):
+            check_signature(self, staticmethod_0, min_args=1)
+        with self.assertRaises(Exception):
+            check_signature(self, staticmethod_1, min_args=2)
+        with self.assertRaises(Exception):
+            check_signature(self, staticmethod_2, min_args=3)
+
+
+class TestWeaklyBoundCallable(unittest.TestCase):
+
+    def setUp(self):
+        self.dictionary = dict()
+
+    def test_unbound(self):
+        def my_func(arg):
+            self.dictionary['my_func'] = arg
+        my_lambda = lambda arg: arg
+
+        weakly = WeaklyBoundCallable(my_func)
+        weakly('arg')
+        self.assertEqual(self.dictionary['my_func'], 'arg')
+        weakly = WeaklyBoundCallable(my_lambda)
+        self.assertEqual(weakly('arg'), 'arg')
+
+    def test_bound(self):
+
+        class MyClass(object):
+
+            def my_method(iself, arg):
+                self.dictionary['my_method'] = (iself, arg)
+                return (iself, arg)
+
+        my_instance = MyClass()
+        my_ref = weakref.ref(my_instance)
+        my_weakly_bound = WeaklyBoundCallable(my_instance.my_method)
+        result = my_weakly_bound('arg')
+        self.assertEqual(self.dictionary['my_method'], (my_instance, 'arg'))
+        self.assertEqual(result, (my_instance, 'arg'))
+        self.assertEqual(my_ref(), my_instance)
+        del my_instance
+        del self.dictionary['my_method']
+        del result
+        gc.collect()
+        self.assertEqual(my_ref(), None)
+
+    def test_equality(self):
+
+        class MyClass(object):
+
+            def method_1(this):
+                pass
+
+            def method_2(this):
+                pass
+
+        instance_1 = MyClass()
+        instance_2 = MyClass()
+        wb_1_1 = WeaklyBoundCallable(instance_1.method_1)
+        wb_1_2 = WeaklyBoundCallable(instance_1.method_2)
+        wb_2_1 = WeaklyBoundCallable(instance_2.method_1)
+        wb_2_2 = WeaklyBoundCallable(instance_2.method_2)
+        self.assertFalse(wb_1_1 == wb_1_2)
+        self.assertFalse(wb_1_1 == wb_2_1)
+        self.assertFalse(wb_1_1 == wb_2_2)
+        self.assertFalse(wb_2_1 == wb_1_2)
+        self.assertFalse(wb_2_1 == wb_2_2)
+        self.assertFalse(wb_2_2 == wb_1_2)
+        self.assertTrue(wb_1_1 != wb_1_2)
+        self.assertTrue(wb_1_1 != wb_2_1)
+        self.assertTrue(wb_1_1 != wb_2_2)
+        self.assertTrue(wb_2_1 != wb_1_2)
+        self.assertTrue(wb_2_1 != wb_2_2)
+        self.assertTrue(wb_2_2 != wb_1_2)
+        wb_1_1_b = WeaklyBoundCallable(instance_1.method_1)
+        self.assertTrue(wb_1_1 == wb_1_1_b)
+        self.assertFalse(wb_1_1 != wb_1_1_b)
+        wb_1_1_c = WeaklyBoundCallable(wb_1_1_b)
+        self.assertTrue(wb_1_1 == wb_1_1_c)
+        self.assertFalse(wb_1_1 != wb_1_1_c)
+        dictionary = dict()
+        dictionary[wb_1_1] = 'wb_1_1'
+        dictionary[wb_1_1_b] = 'wb_1_1_b'
+        dictionary[wb_1_1_c] = 'wb_1_1_c'
+        self.assertEqual(len(dictionary), 1)
+        self.assertEqual(dictionary[wb_1_1], 'wb_1_1_c')
+        self.assertEqual(dictionary[wb_1_1_b], 'wb_1_1_c')
+        self.assertEqual(dictionary[wb_1_1_c], 'wb_1_1_c')
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,4 +1,11 @@
 from collections import Iterable
+from weakref import ref
+from inspect import getargspec, ismethod
+
+
+def none_or_equal(a, b):
+    """ returns True if a is None or a == b """
+    return a is None or a == b
 
 
 def iterablate(target, wrapper_cls=tuple, also_wrap=None):
@@ -8,12 +15,12 @@ def iterablate(target, wrapper_cls=tuple, also_wrap=None):
     or a dict), wrap target inside an iterable.  This can facilitate,
     e.g., bi-modal parameters to functions (i.e., functions that accept
     a value or a list of values in the same parameter).
-    
+
     Keyword arguments:
     target: the object to iterablate
     wrapper_cls: the iterable class that will be used to wrap target
     also_wrap: additional classes to wrap (whether iterable or not)
-    
+
     Return value:
     an iterable, either target (unmodified), or an instance of wrapper_cls
     that contains target
@@ -30,3 +37,98 @@ def iterablate(target, wrapper_cls=tuple, also_wrap=None):
             not isinstance(target, Iterable)):
         return wrapper_cls([target])
     return target
+
+
+def check_signature(_callable, num_args=None, min_args=0, max_args=None):
+    """ Checks the call signature of a provided callable
+
+    Ensures a priori that the callable can accept the specified
+    number(s) of arguments
+
+    Keyword arguments:
+    _callable: the callable that will be inspected
+
+    num_args: exact number of arguments to check function compatibility.
+    If num_args is not None, min_args and max_args are ignored
+
+    min_args, max_args: specify range of argument list lengths to check
+    function compatibility.  A value of None for max_args indicates no upper
+    bound on the number of arguments the callable must accept
+    """
+    if num_args is not None:
+        min_args = num_args
+        max_args = num_args
+    if max_args is not None:
+        assert(max_args >= min_args), \
+            "max_args(%d) < min_args(%d)" % (max_args, min_args)
+
+    argspec = getargspec(_callable)
+    call_arg_num = len(argspec.args) if argspec.args else 0
+    if hasattr(_callable, '__self__'):
+        call_arg_num -= 1
+    defaults_num = len(argspec.defaults) if argspec.defaults else 0
+    required = call_arg_num - defaults_num
+    if required >= 0:
+        assert min_args >= required, \
+            "min_args(%d) < required by callable(%d)" % (min_args, required)
+    if not argspec.varargs:
+        assert max_args is not None, \
+            "max_args is not defined, callable must accept *args"
+        assert max_args <= call_arg_num, \
+            "max_args(%d) > accepted by callable(%d)" % (
+                max_args, call_arg_num)
+
+
+class WeaklyBoundCallable(object):
+
+    """
+    a callable that can be weakly bound to an object
+
+    Normally, a bound function (e.g., a bound method) will retain the
+    bound parameter and prevent garbage collection thereof.  This class
+    simulates the call semantics of a bound function, but only holds
+    a weak reference to the bound object.
+
+    If the wrapped function is a bound method, this will "unbind" it
+    for memory management purposes and "rebind" it on call.
+    """
+
+    def __init__(self, fn):
+        self.__func__ = getattr(fn, '__func__', fn)
+        try:
+            self.__self__ = getattr(fn, '__self__')
+            self._is_bound = True
+        except AttributeError:
+            self._is_bound = False
+
+    @property
+    def __self__(self):
+        return self.__self() if self.__self is not None else None
+
+    @__self__.setter
+    def __self__(self, value):
+        self.__self = ref(value) if value is not None else None
+
+    @__self__.deleter
+    def __self__(self):
+        del self.__self
+
+    def __call__(self, *args, **kwargs):
+        if self._is_bound:
+            return self.__func__(self.__self__, *args, **kwargs)
+        else:
+            return self.__func__(*args, **kwargs)
+
+    def __eq__(self, other):
+        if isinstance(other, WeaklyBoundCallable):
+            f = self.__func__ == getattr(other, '__func__', other)
+            b = self._is_bound == other._is_bound
+            s = self.__self__ == other.__self__
+            return f and b and s
+        return False
+
+    def __ne__(self, other):
+        return not (self.__eq__(other))
+
+    def __hash__(self):
+        return id(self.__func__) ^ id(self.__self__)
